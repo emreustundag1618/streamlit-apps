e6ff9074688e9a28cea74f7846cd119e814652e8
emreustundag1618 <emre.ustundag.1.618@gmail.com>
2022-08-01 16:12:58 +0300

    Update dna-app.py

--- DNA/dna-app.py
+++ DNA/dna-app.py
@@ -21,7 +21,7 @@ sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAG
 sequence = st.text_area("Sequence input", sequence_input, height = 250)
 sequence = sequence.splitlines()
 sequence = sequence[1:] # skips the sequence name (first line)
-sequence = "".join(sequence)
+sequence = "".join(sequence) # concatenates list to string
 
 st.write("""
 ***
